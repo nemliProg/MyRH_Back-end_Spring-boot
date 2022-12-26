f31d91c08d2fa27c8ce657ad0f8a03cf307c9310
nemliProg <younesbuzz@gmail.com>
2022-12-26 16:30:50 +0100

    project initialised

---
+++ MyRH/src/test/java/ma/myrh/MyRhApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.myrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
