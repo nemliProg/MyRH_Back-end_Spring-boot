bd39df7547a663ef295c5eea2e8741f615945c83
nemliProg <younesbuzz@gmail.com>
2023-01-08 17:13:33 +0100

    adding TwilioUtil for helping us to send SMS to companies

---
+++ MyRH/src/main/java/ma/myrh/util/TwilioUtil.java
@@ -0,0 +1,34 @@
+package ma.myrh.util;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class TwilioUtil {
+
+    @Value("${twilio.account.sid}")
+    private String ACCOUNT_SID;
+
+    @Value("${twilio.auth.token}")
+    private String AUTH_TOKEN ;
+
+    @Value("${twilio.phone.number}")
+    private String PHONE_NUMBER;
+
+    public void sendSMS(String to, String body) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        System.out.println(ACCOUNT_SID+"_"+AUTH_TOKEN+"_"+PHONE_NUMBER);
+        Message message = Message.creator(
+                        new PhoneNumber(to),
+                        new PhoneNumber(PHONE_NUMBER),
+                        body)
+                .create();
+        System.out.println(message.getSid());
+    }
+
+}
